8a337d7af42c835b30dc3e77a2ee0d24bf1df539
Autumn Austin <a.austin0119@gmail.com>
2022-02-17 16:01:56 -0500

    First Commit for Money Maker Demo

---
+++ MoneyMaker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Greet the user and get input
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.Write("Enter an amount to convert to coins: ");
+
+            string totalAsString = Console.ReadLine();
+            double total = Convert.ToDouble(totalAsString);
+
+            Console.WriteLine($"{total} is equal to ...");
+
+            // Define coin values
+            int quarterValue = 25;
+            int dimeValue = 10;
+            int nickelValue = 5;
+
+            // Calculate the change
+            double quarters = Math.Floor(total / quarterValue);
+            double remainder = total % quarterValue;
+
+            double dimes = Math.Floor(remainder / dimeValue);
+            remainder = remainder % dimeValue;
+
+            double nickels = Math.Floor(remainder / nickelValue);
+            remainder = remainder % nickelValue;
+
+            // Print the results
+            Console.WriteLine($"Quarters: {quarters}");
+            Console.WriteLine($"Dimes: {dimes}");
+            Console.WriteLine($"Nickels: {nickels}");
+            Console.WriteLine($"Pennies: {remainder}");
+
+
+        
+        }
+    }
+}
